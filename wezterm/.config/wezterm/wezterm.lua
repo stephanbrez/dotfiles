c623eeda795bdffc70e9006fb14ff62b130ed34b
Stephan Brezinsky <me@stephanbrezinsky.com>
2024-10-05 14:49:29 -0400

    keybind to rename tab

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -101,6 +101,21 @@ config.keys = {
 		mods = "CTRL|SHIFT",
 		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
 	},
+	{
+		key = "e",
+		mods = "CTRL|SHIFT",
+		action = act.PromptInputLine({
+			description = "Enter new name for tab",
+			action = wezterm.action_callback(function(window, pane, line)
+				-- line will be `nil` if they hit escape without entering anything
+				-- An empty string if they just hit enter
+				-- Or the actual line of text they wrote
+				if line then
+					window:active_tab():set_title(line)
+				end
+			end),
+		}),
+	},
 	-- Workspaces
 	-- Show the launcher in fuzzy selection mode and have it list all workspaces
 	-- and allow activating one.
