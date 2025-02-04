662664d037c1ac4eeebeca93f5cd9c5426d2056b
Stephan Brezinsky <me@stephanbrezinsky.com>
2025-02-04 13:12:01 -0500

    added obsidian support to blink

--- nvim/.config/nvim/lua/plugins/blink.lua
+++ nvim/.config/nvim/lua/plugins/blink.lua
@@ -1,7 +1,13 @@
--- this is a plugin that makes the completion menu appear
 return {
   "saghen/blink.cmp",
+  dependencies = {
+    { "saghen/blink.compat", lazy = true, version = false },
+  },
   opts = {
+    sources = {
+      -- LazyVim as custom option compat to pass in external sources with blink.compat
+      compat = { "obsidian", "obsidian_new", "obsidian_tags" },
+    },
     keymap = {
       preset = "super-tab",
       ["<C-y>"] = { "select_and_accept" },
