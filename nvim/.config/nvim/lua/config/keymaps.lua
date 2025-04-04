436138a44974819a61fa300bf779c8ad09eadff4
Stephan Brezinsky <me@stephanbrezinsky.com>
2025-04-04 11:51:08 -0400

    feat: added bullets checkbox toggle to which-key via keybind

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -266,6 +266,9 @@ end, { desc = "[P]Toggle bullet point (dash)" })
 -- Open Zoxide telescope extension
 vim.keymap.set("n", "<leader>Z", "<cmd>Zi<CR>", { desc = "Open Zoxide" })
 
+-- Bullets
+vim.keymap.set("n", "<leader>k", "<Plug>(bullets-toggle-checkbox)", { desc = "Toggle Checkbox" })
+
 -- Obsidian
 wk.add({
   { "<leader>o", group = "obsidian", mode = { "n", "v" }, icon = { icon = "󰂺 ", color = "blue" } },
