45c4c1f91ad92e25bd02d0d92378bced54e4f166
Stephan Brezinsky <me@stephanbrezinsky.com>
2025-08-10 18:51:00 -0400

    feat: calling vscode bindings file, fixing indents

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -15,10 +15,10 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 -- Jump between markdown headers
 vim.keymap.set("n", "gj", [[/^##\+ .*<CR>]], { buffer = true, silent = true, desc = "Jump to next markdown header" })
 vim.keymap.set(
-  "n",
-  "gk",
-  [[?^##\+ .*<CR>]],
-  { buffer = true, silent = true, desc = "Jump to previous markdown header" }
+    "n",
+    "gk",
+    [[?^##\+ .*<CR>]],
+    { buffer = true, silent = true, desc = "Jump to previous markdown header" }
 )
 
 -- ###################
@@ -26,43 +26,43 @@ vim.keymap.set(
 -- ###################
 -- Autocmd to store the starting line when entering insert mode
 vim.api.nvim_create_autocmd("InsertEnter", {
-  pattern = "*",
-  callback = function()
-    -- Store the current line number when entering insert mode
-    vim.b.insertLineStart = vim.fn.line(".")
-  end,
+    pattern = "*",
+    callback = function()
+        -- Store the current line number when entering insert mode
+        vim.b.insertLineStart = vim.fn.line(".")
+    end,
 })
 
 -- Function to handle Down arrow key in insert mode
 function InsertModeDown()
-  local current_line = vim.fn.line(".")
-  local insert_line_start = vim.b.insertLineStart or current_line
+    local current_line = vim.fn.line(".")
+    local insert_line_start = vim.b.insertLineStart or current_line
 
-  -- Exit insert mode if cursor moves more than one line down
-  if current_line > insert_line_start + 1 then
-    vim.cmd("stopinsert")
-  end
-  return "<Down>"
+    -- Exit insert mode if cursor moves more than one line down
+    if current_line > insert_line_start + 1 then
+        vim.cmd("stopinsert")
+    end
+    return "<Down>"
 end
 
 -- Function to handle Up arrow key in insert mode
 function InsertModeUp()
-  local current_line = vim.fn.line(".")
-  local insert_line_start = vim.b.insertLineStart or current_line
+    local current_line = vim.fn.line(".")
+    local insert_line_start = vim.b.insertLineStart or current_line
 
-  -- Exit insert mode if cursor moves more than one line up
-  if current_line < insert_line_start - 1 then
-    vim.cmd("stopinsert")
-  end
-  return "<Up>"
+    -- Exit insert mode if cursor moves more than one line up
+    if current_line < insert_line_start - 1 then
+        vim.cmd("stopinsert")
+    end
+    return "<Up>"
 end
 
 -- Key mappings for Up and Down arrows in insert mode
 vim.keymap.set("i", "<Down>", function()
-  return InsertModeDown()
+    return InsertModeDown()
 end, { expr = true })
 vim.keymap.set("i", "<Up>", function()
-  return InsertModeUp()
+    return InsertModeUp()
 end, { expr = true })
 
 -- Select all
@@ -99,36 +99,36 @@ vim.keymap.set("n", ">", "<S-v>><esc>", { desc = "Select line and indent right" 
 -- ###################
 -- open current file in python interactive session
 vim.keymap.set("n", "<leader>t", function()
-  local currentfile = vim.fn.expand("%")
-  vim.cmd("new")
-  vim.cmd("terminal python3 -i " .. currentfile)
+    local currentfile = vim.fn.expand("%")
+    vim.cmd("new")
+    vim.cmd("terminal python3 -i " .. currentfile)
 end, { desc = "Run File in Python Terminal" })
 
 local function rename_terminal()
-  -- Get the current buffer number
-  local bufnr = vim.api.nvim_get_current_buf()
+    -- Get the current buffer number
+    local bufnr = vim.api.nvim_get_current_buf()
 
-  -- Check if the current buffer is a terminal buffer
-  if vim.bo[bufnr].filetype == "terminal" then
-    -- Execute the fc command to get the most recent command from Zsh history
-    local cmd = "fc -l 1 | tail -n 1"
-    local output = vim.fn.system(cmd)
+    -- Check if the current buffer is a terminal buffer
+    if vim.bo[bufnr].filetype == "terminal" then
+        -- Execute the fc command to get the most recent command from Zsh history
+        local cmd = "fc -l 1 | tail -n 1"
+        local output = vim.fn.system(cmd)
 
-    if output ~= "" then
-      -- Remove any trailing characters like spaces or newlines
-      local last_command = string.gsub(output, "%s+$", "")
+        if output ~= "" then
+            -- Remove any trailing characters like spaces or newlines
+            local last_command = string.gsub(output, "%s+$", "")
 
-      -- Escape the command to avoid issues with special characters
-      last_command = vim.fn.fnameescape(last_command)
+            -- Escape the command to avoid issues with special characters
+            last_command = vim.fn.fnameescape(last_command)
 
-      -- Rename the terminal window
-      vim.cmd("title " .. last_command)
+            -- Rename the terminal window
+            vim.cmd("title " .. last_command)
+        else
+            vim.notify("No commands in terminal history", "WARNING")
+        end
     else
-      vim.notify("No commands in terminal history", "WARNING")
+        vim.notify("Not in a terminal buffer", "ERROR")
     end
-  else
-    vim.notify("Not in a terminal buffer", "ERROR")
-  end
 end
 
 -- Bind the function to a key sequence, e.g., <leader>tr
@@ -140,11 +140,11 @@ vim.keymap.set("n", "<leader>wr", rename_terminal, { desc = "Rename Terminal" })
 
 -- which-key menu groups
 wk.add({
-  {
-    mode = { "n", "v" }, -- both normal and visual mode
-    { "<leader>m", group = "markdown", icon = { icon = " ", color = "red" } },
-    { "<leader>mh", group = "headings", icon = { icon = " ", color = "red" } },
-  },
+    {
+        mode = { "n", "v" }, -- both normal and visual mode
+        { "<leader>m", group = "markdown", icon = { icon = " ", color = "red" } },
+        { "<leader>mh", group = "headings", icon = { icon = " ", color = "red" } },
+    },
 })
 
 vim.keymap.set("n", "<leader>md", "<cmd>MarkdownPreviewToggle<CR>", { desc = "Toggle Markdown Preview" })
@@ -161,50 +161,50 @@ vim.keymap.set("n", "<leader>mj", ":/^#\\+ .*<CR>", { desc = "Jump to next markd
 vim.keymap.set("n", "<leader>mk", ":?^#\\+ .*<CR>", { desc = "Jump to previous markdown header" })
 
 vim.keymap.set("n", "<leader>mhi", function()
-  local line = vim.api.nvim_get_current_line() -- Get the current line
-  if line:match("^#+") then -- Check if the line starts with one or more '#'
-    -- Increase heading level by adding another '#'
-    local new_heading = line:gsub("^(#+)", function(h)
-      return h .. "#"
-    end)
-    vim.api.nvim_set_current_line(new_heading) -- Set the new line
-  else
-    -- If it's not a heading, create a new heading
-    vim.api.nvim_set_current_line("# " .. line)
-  end
+    local line = vim.api.nvim_get_current_line() -- Get the current line
+    if line:match("^#+") then                    -- Check if the line starts with one or more '#'
+        -- Increase heading level by adding another '#'
+        local new_heading = line:gsub("^(#+)", function(h)
+            return h .. "#"
+        end)
+        vim.api.nvim_set_current_line(new_heading) -- Set the new line
+    else
+        -- If it's not a heading, create a new heading
+        vim.api.nvim_set_current_line("# " .. line)
+    end
 end, { desc = "Increase Heading Level" })
 
 vim.keymap.set("n", "<leader>mhd", function()
-  local line = vim.api.nvim_get_current_line() -- Get the current line
-  if line:match("^#+") then -- Check if the line starts with one or more '#'
-    -- Decrease heading level by removing one '#'
-    local new_heading = line:gsub("^(#+)", function(h)
-      return h:sub(2) -- Remove the first '#'
-    end)
-    vim.api.nvim_set_current_line(new_heading) -- Set the new line
-  end
+    local line = vim.api.nvim_get_current_line() -- Get the current line
+    if line:match("^#+") then                    -- Check if the line starts with one or more '#'
+        -- Decrease heading level by removing one '#'
+        local new_heading = line:gsub("^(#+)", function(h)
+            return h:sub(2)                        -- Remove the first '#'
+        end)
+        vim.api.nvim_set_current_line(new_heading) -- Set the new line
+    end
 end, { desc = "Decrease Heading Level" })
 
 vim.keymap.set("n", "<leader>mhI", function()
-  -- Save the current cursor position
-  local cursor_pos = vim.api.nvim_win_get_cursor(0)
-  -- I'm using [[ ]] to escape the special characters in a command
-  vim.cmd([[:g/\(^$\n\s*#\+\s.*\n^$\)/ .+1 s/^#\+\s/#&/]])
-  -- Restore the cursor position
-  vim.api.nvim_win_set_cursor(0, cursor_pos)
-  -- Clear search highlight
-  vim.cmd("nohlsearch")
+    -- Save the current cursor position
+    local cursor_pos = vim.api.nvim_win_get_cursor(0)
+    -- I'm using [[ ]] to escape the special characters in a command
+    vim.cmd([[:g/\(^$\n\s*#\+\s.*\n^$\)/ .+1 s/^#\+\s/#&/]])
+    -- Restore the cursor position
+    vim.api.nvim_win_set_cursor(0, cursor_pos)
+    -- Clear search highlight
+    vim.cmd("nohlsearch")
 end, { desc = "Increase ALL headings" })
 
 vim.keymap.set("n", "<leader>mhD", function()
-  -- Save the current cursor position
-  local cursor_pos = vim.api.nvim_win_get_cursor(0)
-  -- I'm using [[ ]] to escape the special characters in a command
-  vim.cmd([[:g/^\s*#\{2,}\s/ s/^#\(#\+\s.*\)/\1/]])
-  -- Restore the cursor position
-  vim.api.nvim_win_set_cursor(0, cursor_pos)
-  -- Clear search highlight
-  vim.cmd("nohlsearch")
+    -- Save the current cursor position
+    local cursor_pos = vim.api.nvim_win_get_cursor(0)
+    -- I'm using [[ ]] to escape the special characters in a command
+    vim.cmd([[:g/^\s*#\{2,}\s/ s/^#\(#\+\s.*\)/\1/]])
+    -- Restore the cursor position
+    vim.api.nvim_win_set_cursor(0, cursor_pos)
+    -- Clear search highlight
+    vim.cmd("nohlsearch")
 end, { desc = "Decrease ALL headings" })
 
 -- ===== Bullets =====
@@ -213,50 +213,50 @@ end, { desc = "Decrease ALL headings" })
 -- "d" is for "dash"
 -- from https://linkarzu.com/posts/neovim/markdown-setup-2025/
 vim.keymap.set("n", "<leader>md", function()
-  -- Get the current cursor position
-  local cursor_pos = vim.api.nvim_win_get_cursor(0)
-  local current_buffer = vim.api.nvim_get_current_buf()
-  local start_row = cursor_pos[1] - 1
-  local col = cursor_pos[2]
-  -- Get the current line
-  local line = vim.api.nvim_buf_get_lines(current_buffer, start_row, start_row + 1, false)[1]
-  -- Check if the line already starts with a bullet point
-  if line:match("^%s*%-") then
-    -- Remove the bullet point from the start of the line
-    line = line:gsub("^%s*%-", "")
-    vim.api.nvim_buf_set_lines(current_buffer, start_row, start_row + 1, false, { line })
-    return
-  end
-  -- Search for newline to the left of the cursor position
-  local left_text = line:sub(1, col)
-  local bullet_start = left_text:reverse():find("\n")
-  if bullet_start then
-    bullet_start = col - bullet_start
-  end
-  -- Search for newline to the right of the cursor position and in following lines
-  local right_text = line:sub(col + 1)
-  local bullet_end = right_text:find("\n")
-  local end_row = start_row
-  while not bullet_end and end_row < vim.api.nvim_buf_line_count(current_buffer) - 1 do
-    end_row = end_row + 1
-    local next_line = vim.api.nvim_buf_get_lines(current_buffer, end_row, end_row + 1, false)[1]
-    if next_line == "" then
-      break
+    -- Get the current cursor position
+    local cursor_pos = vim.api.nvim_win_get_cursor(0)
+    local current_buffer = vim.api.nvim_get_current_buf()
+    local start_row = cursor_pos[1] - 1
+    local col = cursor_pos[2]
+    -- Get the current line
+    local line = vim.api.nvim_buf_get_lines(current_buffer, start_row, start_row + 1, false)[1]
+    -- Check if the line already starts with a bullet point
+    if line:match("^%s*%-") then
+        -- Remove the bullet point from the start of the line
+        line = line:gsub("^%s*%-", "")
+        vim.api.nvim_buf_set_lines(current_buffer, start_row, start_row + 1, false, { line })
+        return
     end
-    right_text = right_text .. "\n" .. next_line
-    bullet_end = right_text:find("\n")
-  end
-  if bullet_end then
-    bullet_end = col + bullet_end
-  end
-  -- Extract lines
-  local text_lines = vim.api.nvim_buf_get_lines(current_buffer, start_row, end_row + 1, false)
-  local text = table.concat(text_lines, "\n")
-  -- Add bullet point at the start of the text
-  local new_text = "- " .. text
-  local new_lines = vim.split(new_text, "\n")
-  -- Set new lines in buffer
-  vim.api.nvim_buf_set_lines(current_buffer, start_row, end_row + 1, false, new_lines)
+    -- Search for newline to the left of the cursor position
+    local left_text = line:sub(1, col)
+    local bullet_start = left_text:reverse():find("\n")
+    if bullet_start then
+        bullet_start = col - bullet_start
+    end
+    -- Search for newline to the right of the cursor position and in following lines
+    local right_text = line:sub(col + 1)
+    local bullet_end = right_text:find("\n")
+    local end_row = start_row
+    while not bullet_end and end_row < vim.api.nvim_buf_line_count(current_buffer) - 1 do
+        end_row = end_row + 1
+        local next_line = vim.api.nvim_buf_get_lines(current_buffer, end_row, end_row + 1, false)[1]
+        if next_line == "" then
+            break
+        end
+        right_text = right_text .. "\n" .. next_line
+        bullet_end = right_text:find("\n")
+    end
+    if bullet_end then
+        bullet_end = col + bullet_end
+    end
+    -- Extract lines
+    local text_lines = vim.api.nvim_buf_get_lines(current_buffer, start_row, end_row + 1, false)
+    local text = table.concat(text_lines, "\n")
+    -- Add bullet point at the start of the text
+    local new_text = "- " .. text
+    local new_lines = vim.split(new_text, "\n")
+    -- Set new lines in buffer
+    vim.api.nvim_buf_set_lines(current_buffer, start_row, end_row + 1, false, new_lines)
 end, { desc = "[P]Toggle bullet point (dash)" })
 
 -- ###################
@@ -271,14 +271,14 @@ vim.keymap.set("n", "<leader>k", "<Plug>(bullets-toggle-checkbox)", { desc = "To
 
 -- Obsidian
 wk.add({
-  { "<leader>o", group = "obsidian", mode = { "n", "v" }, icon = { icon = "󰂺 ", color = "blue" } },
+    { "<leader>o", group = "obsidian", mode = { "n", "v" }, icon = { icon = "󰂺 ", color = "blue" } },
 })
 
 vim.keymap.set(
-  "n",
-  "<leader>oc",
-  "<cmd>lua require('obsidian').util.toggle_checkbox()<CR>",
-  { desc = "Obsidian Check Checkbox" }
+    "n",
+    "<leader>oc",
+    "<cmd>lua require('obsidian').util.toggle_checkbox()<CR>",
+    { desc = "Obsidian Check Checkbox" }
 )
 vim.keymap.set("n", "<leader>ot", "<cmd>ObsidianTemplate<CR>", { desc = "Insert Obsidian Template" })
 vim.keymap.set("n", "<leader>oo", "<cmd>ObsidianOpen<CR>", { desc = "Open in Obsidian App" })
@@ -294,38 +294,43 @@ vim.keymap.set("n", "<leader>oq", "<cmd>ObsidianQuickSwitch<CR>", { desc = "Quic
 
 -- Zen Mode
 wk.add({
-  { "<leader>z", group = "zen-mode", icon = { icon = " ", color = "grey" } },
+    { "<leader>z", group = "zen-mode", icon = { icon = " ", color = "grey" } },
 })
 
 vim.keymap.set("n", "<leader>zh", function()
-  require("zen-mode").toggle({
-    window = {
-      width = 120,
-      options = {
-        -- signcolumn = "no", -- disable signcolumn
-        number = false, -- disable number column
-        relativenumber = false, -- disable relative numbers
-        -- cursorline = false, -- disable cursorline
-        -- cursorcolumn = false, -- disable cursor column
-        -- foldcolumn = "0", -- disable fold column
-        -- list = false, -- disable whitespace characters
-      },
-    },
-  })
+    require("zen-mode").toggle({
+        window = {
+            width = 120,
+            options = {
+                -- signcolumn = "no", -- disable signcolumn
+                number = false,         -- disable number column
+                relativenumber = false, -- disable relative numbers
+                -- cursorline = false, -- disable cursorline
+                -- cursorcolumn = false, -- disable cursor column
+                -- foldcolumn = "0", -- disable fold column
+                -- list = false, -- disable whitespace characters
+            },
+        },
+    })
 end, { desc = "Toggle Zen Mode without line numbers" })
 vim.keymap.set("n", "<leader>zl", function()
-  require("zen-mode").toggle({
-    window = {
-      width = 120,
-      options = {
-        -- signcolumn = "no", -- disable signcolumn
-        number = true, -- enable number column
-        relativenumber = true, -- enable relative numbers
-        -- cursorline = false, -- disable cursorline
-        -- cursorcolumn = false, -- disable cursor column
-        -- foldcolumn = "0", -- disable fold column
-        -- list = false, -- disable whitespace characters
-      },
-    },
-  })
+    require("zen-mode").toggle({
+        window = {
+            width = 120,
+            options = {
+                -- signcolumn = "no", -- disable signcolumn
+                number = true,         -- enable number column
+                relativenumber = true, -- enable relative numbers
+                -- cursorline = false, -- disable cursorline
+                -- cursorcolumn = false, -- disable cursor column
+                -- foldcolumn = "0", -- disable fold column
+                -- list = false, -- disable whitespace characters
+            },
+        },
+    })
 end, { desc = "Toggle Zen Mode with Line Numbers" })
+
+if vim.g.vscode then
+    print("⚡connected to neovim!💯‼️🤗😎")
+    require "vscode_keymaps"
+end
