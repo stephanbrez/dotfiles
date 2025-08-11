a572d7312508ba9e286f99548f898745320841c9
Stephan Brezinsky <me@stephanbrezinsky.com>
2025-08-10 21:19:34 -0400

    feat: adding groups in separate file

---
+++ nvim/.config/nvim/lua/plugins/which-key.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "folke/which-key.nvim",
+    opts = {
+      defaults = {},
+      spec = {
+        {
+          { "<leader>z", group = "zen-mode", icon = { icon = " ", color = "grey" } },
+          { "<leader>m", group = "markdown", mode = { "n", "v" }, icon = { icon = " ", color = "red" } },
+          { "<leader>mh", group = "headings", mode = { "n", "v" }, icon = { icon = " ", color = "red" } },
+          { "<leader>o", group = "obsidian", mode = { "n", "v" }, icon = { icon = "󰂺 ", color = "blue" } },
+        },
+      },
+    },
+  },
+}
