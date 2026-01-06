b3ab1a5605f4f21486786f368a728caa34572f84
Stephan Brezinsky <me@stephanbrezinsky.com>
2026-01-05 23:31:56 -0500

    feat: latex

--- nvim/.config/nvim/lua/plugins/render-markdown.lua
+++ nvim/.config/nvim/lua/plugins/render-markdown.lua
@@ -8,6 +8,9 @@ return {
     ---@module 'render-markdown'
     ---@type render.md.UserConfig
     opts = {
+      latex = {
+        enabled = true,
+      },
       code = {
         sign = true,
       },
