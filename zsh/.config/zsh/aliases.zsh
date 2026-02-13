910f3a2412f52c459240372aaeccfeaf4806b83e
Stephan Brezinsky <me@stephanbrezinsky.com>
2026-02-13 14:17:31 -0500

    feat: better wezterm connect

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -450,7 +450,10 @@ alias sitecopy='wget -k -K -E -r -l 10 -p -N -F -nH '
 # --- Wezterm ---
 # Usage: wtc <hostname> or just use host-specific aliases below
 wtc() {
-    wezterm connect --new-tab "$1"                  # Connect to host in new tab
+    wezterm cli spawn --domain-name  SSHMUX:"$1"    # Connect to wezterm on host from .ssh/config in new tab
+}
+wts() {
+    wezterm connect SSH:"$1"                        # Connect to SSH host from .ssh/config in new window
 }
 alias wtd="wezterm disconnect"                      # Disconnect from wezterm session
 alias wtl="wezterm cli list"                        # List all windows tabs and panes
