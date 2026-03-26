982d83a4698b4964559da48f4f0aa3f0591f4c5b
Stephan Brezinsky <me@stephanbrezinsky.com>
2026-03-26 13:34:21 -0400

    feat: adding pixi support for xdg spec

--- zsh/.config/zsh/zprofile.d/20-dev-server-env.zsh
+++ zsh/.config/zsh/zprofile.d/20-dev-server-env.zsh
@@ -1,4 +1,20 @@
 # Dev server-specific environment
 
-# Path
-export PATH="/usr/local/cuda/bin/:$PATH"
+###########################
+#          TOOLS          #
+###########################
+# --- Pixi: relocate everything ---
+export PIXI_HOME="$XDG_DATA_HOME/pixi"
+export PIXI_CACHE_DIR="$XDG_CACHE_HOME/pixi"
+
+###########################
+#          PATHS          #
+###########################
+export PATH="/usr/local/cuda/bin/:$PATH"     # nvcc binary
+# --- Ensure pixi binary is on PATH ---
+case ":$PATH:" in
+  *":$PIXI_HOME/bin:"*) ;;
+  *) export PATH="$PIXI_HOME/bin:$PATH" ;;
+esac
+
+
