1364794eb5b867a1fd1f74cab981ec6e3d32c08e
Stephan Brezinsky <me@stephanbrezinsky.com>
2026-08-25 19:44:53 -0400

    fix(zprofile): set cache home for macs

--- zsh/.config/zsh/zprofile.d/00-base-env.zsh
+++ zsh/.config/zsh/zprofile.d/00-base-env.zsh
@@ -29,6 +29,7 @@ export LANG=en_US.UTF-8
 export XDG_CONFIG_HOME="$HOME/.config"
 export XDG_DATA_HOME="$HOME/.local/share"
 export XDG_STATE_HOME="$HOME/.local/state"
+export XDG_CACHE_HOME="$HOME/.cache"
 
 # ZDOTDIR is set in ~/.zprofile (the entry point)
 # This file is sourced after ZDOTDIR is already set
